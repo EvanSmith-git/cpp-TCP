fb86c0fd8a918d22944d70f9d16ef6c6162fb855
EvanSmith-git <EvanSmith-git@users.noreply.github.com>
2021-08-22 23:00:33 -0400

    Test/Example for client in Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "TCP/Client/Client.h"
 
 int main() {
-	std::cout << "Hello World!\n";
+	TCP::Client myClient("127.0.0.1", "678", [](TCP::Connection* con){
+		tcpData msg;
+		msg.data = std::make_unique<char[]>(12);
+		msg.size = 12;
+		memcpy(msg.data.get(), "Hello World", 12);
+
+		con->send(msg);
+	});
+
 	return 0;
 }
