f7a1e9c2ad8bbdce9cc0cad4b67af0c0b451cc94
EvanSmith-git <EvanSmith-git@users.noreply.github.com>
2021-09-11 21:19:52 -0400

    Rename struct tcpData -> TcpData

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 	TCP::Client myClient("127.0.0.1", "678", [](TCP::Connection* con){
-		tcpData msg;
+		TcpData msg;
 		msg.data = std::make_unique<char[]>(12);
 		msg.size = 12;
 		memcpy(msg.data.get(), "Hello World", 12);
