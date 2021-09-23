2ed618cd1aba9d0258fb737a368b4851ebd2d36b
EvanSmith-git <EvanSmith-git@users.noreply.github.com>
2021-09-23 14:56:36 -0400

    Option to supply errCode to getErrorText

--- TCP/Utility/Utility.cpp
+++ TCP/Utility/Utility.cpp
@@ -6,18 +6,18 @@
 #include <WS2tcpip.h>
 #endif
 
-std::string getErrorText() {
+std::string getErrorText(int errCode = 0) {
 #if defined(_WIN32)
     static char message[256] = { 0 };
     FormatMessageA(
         FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-        0, WSAGetLastError(), 0, message, 256, 0);
+        0, errCode ? errCode : WSAGetLastError(), 0, message, 256, 0);
     char* nl = strrchr(message, '\n');
     if (nl) *nl = 0;
     return message;
 
 #else
-    return strerror(errno);
+    return strerror(errCode ? errCode : errno);
 
 #endif
 }
--- TCP/Utility/Utility.h
+++ TCP/Utility/Utility.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <string>
 
-std::string getErrorText();
+std::string getErrorText(int errCode = 0);
 std::string ipFromAddrinfo(const sockaddr* p);
 std::string portFromAddrinfo(const sockaddr* p);
