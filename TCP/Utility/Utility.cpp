d0bbaf86f5e131f9d049b56967d0bf2249a42f4d
EvanSmith-git <EvanSmith-git@users.noreply.github.com>
2021-09-23 14:28:59 -0400

    ip and port properties

--- TCP/Utility/Utility.cpp
+++ TCP/Utility/Utility.cpp
@@ -1,6 +1,9 @@
 #include "Utility.h"
 #if defined(_WIN32)
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
 #include <Windows.h>
+#include <WS2tcpip.h>
 #endif
 
 std::string getErrorText() {
@@ -17,4 +20,30 @@ std::string getErrorText() {
     return strerror(errno);
 
 #endif
+}
+
+std::string ipFromAddrinfo(const sockaddr* p) {
+    char ipstr[INET6_ADDRSTRLEN];
+
+    void* addrData = p->sa_family == AF_INET ?
+        // IPv4
+        (void*)&((sockaddr_in*)p)->sin_addr :
+        // IPv6
+        (void*)&((sockaddr_in6*)p)->sin6_addr;
+
+    inet_ntop(p->sa_family, addrData, ipstr, sizeof ipstr);
+
+    return std::string(ipstr);
+}
+
+std::string portFromAddrinfo(const sockaddr* p) {
+    char ipstr[INET6_ADDRSTRLEN];
+
+    USHORT port = p->sa_family == AF_INET ?
+        // IPv4
+        ((sockaddr_in*)p)->sin_port :
+        // IPv6
+        ((sockaddr_in6*)p)->sin6_port;
+
+    return std::to_string(port);
 }
--- TCP/Utility/Utility.h
+++ TCP/Utility/Utility.h
@@ -2,3 +2,5 @@
 #include <string>
 
 std::string getErrorText();
+std::string ipFromAddrinfo(const sockaddr* p);
+std::string portFromAddrinfo(const sockaddr* p);
